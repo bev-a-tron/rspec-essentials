8f1938cb9f49120430674e5e3c499ee209daac5a
Beverly Lau <beverly.a.lau@gmail.com>
2016-07-26 11:22:22 -0700

    Add custom matcher

---
+++ customer_2_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require_relative 'customer'
+
+RSpec::Matchers.define :be_discounted do |product, expected|
+  match do |customer|
+    customer.discount_amount_for(product) == expected
+  end
+end
+
+describe "product discount" do
+  let(:product)      { "foo123"                           }
+  let(:discounts)    { { product => 0.1 }                 }
+  subject(:customer) { Customer.new(discounts: discounts) }
+
+  it "detects when customer has a discount" do
+    expect(customer).to be_discounted(product, 0.1)
+  end
+end
