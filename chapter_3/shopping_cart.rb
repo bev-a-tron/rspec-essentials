c35275a02d38aa298258f1e8eb348a4a950e23b3
Beverly Lau <beverly.a.lau@gmail.com>
2016-08-03 10:55:22 -0700

    Use Object.new, use rspec double

--- chapter_3/shopping_cart.rb
+++ chapter_3/shopping_cart.rb
@@ -40,4 +40,29 @@ RSpec.describe ShoppingCart do
       expect(cart.total_price).to eq(num_products * price)
     end
   end
+
+  context "using Object.new" do
+    it "returns the sum of the prices of all products" do
+      num_products  = 22
+      price         = 100
+      cart          = ShoppingCart.new
+      product       = Object.new
+      some_products = [product] * num_products
+      expect(cart).to receive(:products).and_return(some_products)
+      allow(product).to receive(:price).and_return(price)
+      expect(cart.total_price).to eq(num_products * price)
+    end
+  end
+
+  context "using RSpec's double" do
+    it "returns the sum of the prices of all products" do
+      num_products  = 22
+      price         = 100
+      cart          = ShoppingCart.new
+      product       = double('Product', price: price)
+      some_products = [product] * num_products
+      expect(cart).to receive(:products).and_return(some_products)
+      expect(cart.total_price).to eq(num_products * price)
+    end
+  end
 end
